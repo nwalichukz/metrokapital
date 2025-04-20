1a9383701d041c981c7a6184caf98049474f519c
unknown <nwalichukz@github.com>
2025-04-20 11:34:09 +0100

    yes updates on views

--- resources/views/dashboard/src/html/components/forms/external-transfer.blade.php
+++ resources/views/dashboard/src/html/components/forms/external-transfer.blade.php
@@ -13,18 +13,11 @@
                                             </div>
                                             <form action="#">
                                                
-                                                
-                                                            <div class="form-group">
-                                                                <label class="form-label" for="fv-topics">Select Bank</label>
-                                                                <div class="form-control-wrap ">
-                                                                    <select class="form-select js-select2" id="fv-topics" name="fv-topics" data-placeholder="Select a option" required>
-                                                                        <option label="empty" value="">Select Bank</option>
-                                                                        <option value="fv-gq">Access Bank</option>
-                                                                        <option value="fv-tq">Ecobank</option>
-                                                                        <option value="fv-ab">UBA</option>
-                                                                    </select>
-                                                                </div>
-                                                            </div>
+                                            <div class="form-group">
+                                                    <label class="form-label" for="cf-email-address">Enter Bank Name</label>
+                                                    <input type="text" class="form-control" id="cf-email-address">
+                                                </div>
+                                                            
                                                       
                                                 <div class="form-group">
                                                     <label class="form-label" for="cf-email-address">Account Number</label>
