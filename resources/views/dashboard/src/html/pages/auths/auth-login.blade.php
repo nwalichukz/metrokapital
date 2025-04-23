7d8279d9b21c8e0ae53f2729ab525c8bca96001d
unknown <nwalichukz@github.com>
2025-04-23 09:23:45 +0100

    ok image corrected

--- resources/views/dashboard/src/html/pages/auths/auth-login.blade.php
+++ resources/views/dashboard/src/html/pages/auths/auth-login.blade.php
@@ -30,7 +30,7 @@
                         <div class="brand-logo pb-4 text-center">
                             <a href="{{url('/')}}" class="logo-link">
                             <img class="logo-light logo-img logo-img-lg" src="{{ asset('dashboard/images/logo.png')}}" srcset="./images/logo.png 2x" alt="logo">
-                            <img class="logo-dark logo-img logo-img-lg" src="{{ asset('dashboard/images/logo.png')}}" srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
+                            <img class="logo-dark logo-img logo-img-lg" src="{{ asset('dashboard/images/logo.png')}}" srcset="./images/logo.png 2x" alt="logo">
                             </a>
                         </div>
                         @include('partials/errors')
